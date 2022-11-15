73f6d72410e7fccb654625d8df08d186186d8e55
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-15 19:54:15 +0900

    검색 바 제작

--- frontEnd/components/Header.tsx
+++ frontEnd/components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import GoBackButton from "@components/GoBackButton";
 import { useRouter } from "next/router";
+import SearchBar from "@components/SearchBar";
 
 export default function Header() {
   const router = useRouter();
@@ -15,44 +16,52 @@ export default function Header() {
   const inSetting = ["/userinfo"];
 
   return (
-    <div className="fixed bg-white w-[375px] h-[60px] flex justify-between p-4 bg-white z-10 shadow">
-      {isNotGoBack.includes(router.pathname) ? null : <GoBackButton />}
-      <div className="flex gap-1 justify-center items-center">
-        {/*Logo + AppName*/}
-        <Link href="/">
-          <a>
-            <div className="flex flex-row items-center">
-              <Image src="/header/logo.svg" width="34px" height="34px" />
-              <Image src="/header/appName.svg" width="92px" height="26px" />
-            </div>
-          </a>
-        </Link>
-      </div>
+    <div>
+      {router.pathname === "/scholarship/search/detail" ? (
+        <div className={"py-2.5 px-4 border-b"}>
+          <SearchBar />
+        </div>
+      ) : (
+        <div className="fixed bg-white w-[375px] h-[60px] flex justify-between p-4 bg-white z-10 shadow">
+          {isNotGoBack.includes(router.pathname) ? null : <GoBackButton />}
+          <div className="flex gap-1 justify-center items-center">
+            {/*Logo + AppName*/}
+            <Link href="/">
+              <a>
+                <div className="flex flex-row items-center">
+                  <Image src="/header/logo.svg" width="34px" height="34px" />
+                  <Image src="/header/appName.svg" width="92px" height="26px" />
+                </div>
+              </a>
+            </Link>
+          </div>
 
-      <div className="flex gap-4 justify-center items-center ">
-        {/*searchIco*/}
-        {inNotSearch.includes(router.pathname) ? null : (
-          <Link href="/scholarship/search/detail">
-            <Image
-              src="/header/searchScholarship.svg"
-              width="30px"
-              height="30px"
-            />
-          </Link>
-        )}
+          <div className="flex gap-4 justify-center items-center ">
+            {/*searchIco*/}
+            {inNotSearch.includes(router.pathname) ? null : (
+              <Link href="/scholarship/search/detail">
+                <Image
+                  src="/header/searchScholarship.svg"
+                  width="30px"
+                  height="30px"
+                />
+              </Link>
+            )}
 
-        {/*notificationIco*/}
-        <Link href="/userinfo/notification">
-          <Image src="/header/notification.svg" width="30px" height="30px" />
-        </Link>
+            {/*notificationIco*/}
+            <Link href="/userinfo/notification">
+              <Image src="/header/notification.svg" width="30px" height="30px" />
+            </Link>
 
-        {/*setting */}
-        {inSetting.includes(router.pathname) && (
-          <Link href="/userinfo/setting">
-            <Image src="/header/setting.svg" width="30px" height="30px" />
-          </Link>
-        )}
-      </div>
+            {/*setting */}
+            {inSetting.includes(router.pathname) && (
+              <Link href="/userinfo/setting">
+                <Image src="/header/setting.svg" width="30px" height="30px" />
+              </Link>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
