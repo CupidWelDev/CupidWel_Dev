143d784892835bc53cb24bd2a6ab596fc16034dd
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-21 23:06:11 +0900

    [ADD] 설정 페이지(/userinfo/setting) [타이틀] 섹션 생성

--- frontEnd/pages/userinfo/setting.tsx
+++ frontEnd/pages/userinfo/setting.tsx
@@ -1,10 +1,22 @@
 import {NextPage} from "next";
+import SEO from "@components/SEO";
 
-const Setting :NextPage = () => {
+const Setting: NextPage = () => {
 
-    return (
-        <div>설정창</div>
-    )
+	return (
+		<div>
+			<SEO title={"설정"} />
+
+			{/* 타이틀 */}
+			<section>
+				<p className="text-2xl text-center font-bold px-3 py-5">
+					설정
+				</p>
+			</section>
+
+
+		</div>
+	)
 }
 
 export default Setting
