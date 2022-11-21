12ae3bb8dd62de734b8f31c6ae98f8251bfc9ce2
Seung-ho Kim <rhanfwlq980@naver.com>
2022-11-21 23:48:14 +0900

    [ADD] 설정 페이지(/userinfo/setting) [메뉴] 섹션 생성

--- frontEnd/pages/userinfo/setting.tsx
+++ frontEnd/pages/userinfo/setting.tsx
@@ -1,5 +1,6 @@
 import {NextPage} from "next";
 import SEO from "@components/SEO";
+import Temp from "public/userInfo/temp.svg";
 
 const Setting: NextPage = () => {
 
@@ -14,7 +15,38 @@ const Setting: NextPage = () => {
 				</p>
 			</section>
 
-
+			{/* TODO : SVG 이미지 파일 수정 */}
+			{/* 메뉴 */}
+			<section className={"px-5 space-y-5"}>
+				{/* 마케팅 알림 수신 */}
+				<button className={"w-full"}>
+					<div className={"flex items-center"}>
+						<Temp />
+						<p className={"mx-5 text-xl"}>마케팅 알림 수신</p>
+					</div>
+				</button>
+				{/* 개인 정보 변경 */}
+				<button className={"w-full"}>
+					<div className={"flex items-center"}>
+						<Temp />
+						<p className={"mx-5 text-xl"}>개인 정보 변경</p>
+					</div>
+				</button>
+				{/* 로그아웃 */}
+				<button className={"w-full"}>
+					<div className={"flex items-center"}>
+						<Temp />
+						<p className={"mx-5 text-xl"}>로그아웃</p>
+					</div>
+				</button>
+				{/* 회원 탈퇴 */}
+				<button className={"w-full"}>
+					<div className={"flex items-center"}>
+						<Temp />
+						<p className={"mx-5 text-xl"}>마케팅 알림 수신</p>
+					</div>
+				</button>
+			</section>
 		</div>
 	)
 }
