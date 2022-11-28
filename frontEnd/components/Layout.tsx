accd39967feae43229e8168149873f97ebfc1310
hun2__2 <wognskec@hanyang.ac.kr>
2022-11-28 13:16:25 +0900

    push notification 성공ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ

--- frontEnd/components/Layout.tsx
+++ frontEnd/components/Layout.tsx
@@ -4,14 +4,14 @@ import PushNotificationLayout from "@components/PushNotificationLayout";
 
 export default function Layout({ children }: any): JSX.Element {
   return (
-    <PushNotificationLayout>
-      <div className="flex justify-center">
-        <div className="w-full sm:w-[400px] min-h-screen bg-white">
-          <Header />
-          <div className="mt-[70px] mb-[64px]">{children}</div>
-          <NavBar />
-        </div>
+    // <PushNotificationLayout>
+    <div className="flex justify-center">
+      <div className="w-full sm:w-[400px] min-h-screen bg-white">
+        <Header />
+        <div className="mt-[70px] mb-[64px]">{children}</div>
+        <NavBar />
       </div>
-    </PushNotificationLayout>
+    </div>
+    // </PushNotificationLayout>
   );
 }
